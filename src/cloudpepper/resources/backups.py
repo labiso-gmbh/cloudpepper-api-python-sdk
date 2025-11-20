5a0bab9cd3ee080aa92cdb17a54a83bc6f0b36d9
Laurence Labusch <38892451+llabusch93@users.noreply.github.com>
2025-11-20 16:32:14 +0100

    feat: update backups resource with models

--- src/cloudpepper/resources/backups.py
+++ src/cloudpepper/resources/backups.py
@@ -1,18 +1,21 @@
+from typing import List
 from ..client import Cloudpepper
 from .base import APIResource
+from ..models import Backup, BackupSchedule
 
 class Backups(APIResource):
     def __init__(self, client: Cloudpepper):
         super().__init__(client.client)
 
-    async def list(self):
-        return await self._get("/backups")
+    async def list(self) -> List[Backup]:
+        data = await self._get("/backups")
+        return [Backup(**item) for item in data]
 
-    async def get(self, backup_id: str):
-        return await self._get(f"/backups/{backup_id}")
+    async def get(self, backup_id: str) -> Backup:
+        return Backup(**await self._get(f"/backups/{backup_id}"))
 
-    async def update(self, backup_id: str, **kwargs):
-        return await self._patch(f"/backups/{backup_id}", json=kwargs)
+    async def update(self, backup_id: str, **kwargs) -> Backup:
+        return Backup(**await self._patch(f"/backups/{backup_id}", json=kwargs))
 
     async def delete(self, backup_id: str):
         return await self._delete(f"/backups/{backup_id}")
@@ -21,11 +24,11 @@ class BackupSchedules(APIResource):
     def __init__(self, client: Cloudpepper):
         super().__init__(client.client)
 
-    async def add(self, instance_id: str, **kwargs):
-        return await self._post(f"/instances/{instance_id}/backup-schedules", json=kwargs)
+    async def add(self, instance_id: str, **kwargs) -> BackupSchedule:
+        return BackupSchedule(**await self._post(f"/instances/{instance_id}/backup-schedules", json=kwargs))
     
-    async def update(self, instance_id: str, schedule_id: str, **kwargs):
-        return await self._patch(f"/instances/{instance_id}/backup-schedules/{schedule_id}", json=kwargs)
+    async def update(self, instance_id: str, schedule_id: str, **kwargs) -> BackupSchedule:
+        return BackupSchedule(**await self._patch(f"/instances/{instance_id}/backup-schedules/{schedule_id}", json=kwargs))
 
     async def delete(self, instance_id: str, schedule_id: str):
         return await self._delete(f"/instances/{instance_id}/backup-schedules/{schedule_id}")
