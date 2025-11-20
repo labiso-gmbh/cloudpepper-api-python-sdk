3ca55f773a525a2144cb90ae0e1af15d9c164905
Laurence Labusch <38892451+llabusch93@users.noreply.github.com>
2025-11-20 16:32:27 +0100

    feat: update users resource with models

--- src/cloudpepper/resources/users.py
+++ src/cloudpepper/resources/users.py
@@ -1,21 +1,24 @@
+from typing import List
 from ..client import Cloudpepper
 from .base import APIResource
+from ..models import User
 
 class Users(APIResource):
     def __init__(self, client: Cloudpepper):
         super().__init__(client.client)
 
-    async def list(self):
-        return await self._get("/users")
+    async def list(self) -> List[User]:
+        data = await self._get("/users")
+        return [User(**item) for item in data]
 
-    async def create(self, **kwargs):
-        return await self._post("/users", json=kwargs)
+    async def create(self, **kwargs) -> User:
+        return User(**await self._post("/users", json=kwargs))
 
-    async def get(self, user_id: str):
-        return await self._get(f"/users/{user_id}")
+    async def get(self, user_id: str) -> User:
+        return User(**await self._get(f"/users/{user_id}"))
 
-    async def update(self, user_id: str, **kwargs):
-        return await self._put(f"/users/{user_id}", json=kwargs)
+    async def update(self, user_id: str, **kwargs) -> User:
+        return User(**await self._put(f"/users/{user_id}", json=kwargs))
 
     async def delete(self, user_id: str):
         return await self._delete(f"/users/{user_id}")
