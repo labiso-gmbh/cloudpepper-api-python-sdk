ac4791304588ee55b386ed14d86f4e2ebb9f5487
Laurence Labusch <38892451+llabusch93@users.noreply.github.com>
2025-11-20 16:32:35 +0100

    feat: update instance_templates resource with models

--- src/cloudpepper/resources/instance_templates.py
+++ src/cloudpepper/resources/instance_templates.py
@@ -1,12 +1,14 @@
+from typing import List
 from ..client import Cloudpepper
 from .base import APIResource
+from ..models import InstanceTemplate
 
 class InstanceTemplates(APIResource):
     def __init__(self, client: Cloudpepper):
         super().__init__(client.client)
 
-    async def create(self, **kwargs):
-        return await self._post("/instance-templates", json=kwargs)
+    async def create(self, **kwargs) -> InstanceTemplate:
+        return InstanceTemplate(**await self._post("/instance-templates", json=kwargs))
 
     async def set_default(self, template_id: str, **kwargs):
         return await self._put(f"/instance-templates/{template_id}/default", json=kwargs)
